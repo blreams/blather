cef5b74c408c674cfb5fdd5ab649c6535be8cf58
blreams <blreams@gmail.com>
2016-08-09 09:10:12 -0400

    Made it through some of the views sessions.

--- blat/views.py
+++ blat/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Blat
+
+
+class IndexView(generic.ListView):
+    template_name = 'blat/home.html'
+    context_object_name = 'blat_list'
+
+    def get_queryset(self):
+        return Blat.objects.order_by('-created_on')[:20]
+
+class DetailView(generic.DetailView):
+    model = Blat
+    template_name = 'blat/detail.html'
+    context_object_name = 'blat'
+
